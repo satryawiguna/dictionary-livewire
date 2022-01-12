b5dc09d5f6eb488430a3dd13123a77ceaedb49e2
Satrya Wiguna <satrya@freshcms.net>
2022-01-12 11:02:37 +0800

    add missing update files

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function dictionaries()
+    {
+        return $this->hasMany(Dictionary::class, 'category_id');
+    }
 }
--- app/Models/Dictionary.php
+++ app/Models/Dictionary.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Dictionary extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'category_id',
+        'vocab_id',
+        'vocab_en'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
